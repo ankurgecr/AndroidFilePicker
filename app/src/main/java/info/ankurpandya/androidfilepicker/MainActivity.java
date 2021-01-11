5ed6ec661324a0ee5de2f99e1713428a640c4e5b
Ankur Pandya <ankurgecr@users.noreply.github.com>
2021-01-11 16:18:06 +0800

    Adding simple file picker option

--- app/src/main/java/info/ankurpandya/androidfilepicker/MainActivity.java
+++ app/src/main/java/info/ankurpandya/androidfilepicker/MainActivity.java
@@ -1,22 +1,48 @@
 package info.ankurpandya.androidfilepicker;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import info.ankurpandya.filepicker.FilePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FilePicker.FilePickerResult {
 
+    FilePicker filePicker;
     TextView text;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = findViewById(R.id.text);
+        image = findViewById(R.id.image);
 
-        text.setText(FilePicker.hello());
+        filePicker = new FilePicker.Builder(this)
+                .setFilePickerResult(this)
+                .build();
+
+        text.setOnClickListener(view -> filePicker.pick());
+    }
+
+    @Override
+    public void onFilePicked(Uri uri) {
+        InputStream inputStream = null;
+        try {
+            inputStream = getApplicationContext().getContentResolver().openInputStream(uri);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            image.setImageBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- filepicker/src/main/java/info/ankurpandya/filepicker/FilePicker.java
+++ filepicker/src/main/java/info/ankurpandya/filepicker/FilePicker.java
@@ -1,11 +1,73 @@
 package info.ankurpandya.filepicker;
 
+import android.net.Uri;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
 /**
  * Create by Ankur @ Worktable.sg
  */
-public class FilePicker {
+public class FilePicker implements DefaultLifecycleObserver {
+    private final ActivityResultRegistry mRegistry;
+    private ActivityResultLauncher<String> mGetContent;
+    private Builder builder;
+    //private ActivityResultLauncher<Intent> mStartForResult;
 
-    public static String hello() {
-        return "world!";
+    public FilePicker(@NonNull ActivityResultRegistry registry) {
+        mRegistry = registry;
+    }
+
+    @Override
+    public void onCreate(@NonNull LifecycleOwner owner) {
+        mGetContent = mRegistry.register("a", owner, new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                        handleActivityResult(uri);
+                    }
+                });
+    }
+
+    private void handleActivityResult(Uri uri) {
+        if (builder.filePickerResult != null) {
+            builder.filePickerResult.onFilePicked(uri);
+        }
+    }
+
+    public void pick() {
+        mGetContent.launch("image/*");
+    }
+
+    public static class Builder {
+
+        private final AppCompatActivity activity;
+        private FilePickerResult filePickerResult;
+
+        public Builder(AppCompatActivity activity) {
+            this.activity = activity;
+        }
+
+        public Builder setFilePickerResult(FilePickerResult filePickerResult) {
+            this.filePickerResult = filePickerResult;
+            return this;
+        }
+
+        public FilePicker build() {
+            FilePicker filePicker = new FilePicker(activity.getActivityResultRegistry());
+            filePicker.builder = this;
+            activity.getLifecycle().addObserver(filePicker);
+            return filePicker;
+        }
+    }
+
+    public interface FilePickerResult {
+        void onFilePicked(Uri uri);
     }
 }
